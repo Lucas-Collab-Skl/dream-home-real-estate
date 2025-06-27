c3b041a7757df15e18fed9af1c80aaefd89de260
Lucas <lukevdm64@gmail.com>
2025-06-27 18:35:23 -0400

    basic auth, ready to be connected to db. Db connection working. Some components and user context setup

---
+++ src/app/types.ts
@@ -0,0 +1,14 @@
+export type Property = {
+    propertyNo: string;
+    street: string;
+    city: string;
+    postalCode: string;
+    type: string;
+    rooms: number;
+    rent: number;
+    ownerNo: string;
+    staffNo: string;
+    branchNo: string;
+    picture: string;
+    floorplan: string;
+}
