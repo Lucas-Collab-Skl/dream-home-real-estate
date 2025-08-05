96a1797c2e9929db10fe6995728e94a7eacb3988
Lucas <lukevdm64@gmail.com>
2025-08-05 15:09:26 -0400

    fixing the user context back to using local storage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,13 +18,12 @@ import { useUser } from "@/app/userContext";
 import { useDisclosure } from "@heroui/react";
 import {useTheme} from "next-themes";
 import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi";
-import { Staff } from "@/app/types";
 
 export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const { isOpen: isLoginOpen, onOpen: onLoginOpen, onClose: onLoginClose } = useDisclosure();
-    const { theme, setTheme } = useTheme()
-    const [user, setUser] = useState<Staff | null>(null);
+    const { user, setUser } = useUser();
+    const { theme, setTheme } = useTheme();
 
     const logout = () => {
         setUser(null);
